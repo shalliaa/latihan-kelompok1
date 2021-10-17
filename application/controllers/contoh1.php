1a42a18a647a5f27cc1e9802dcb30a0b14fd72df
shalliaa <shalliaayuthahirakum@gmail.com>
2021-10-17 21:45:18 +0700

    mengerjakkan modul pertemuan 2 membuat contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Shallia Yuthahirakum
+        Saya tinggal di daerah Tanuwijaya
+        Olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
